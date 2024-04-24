86db8e2eae2690e00aeacbc005ff7fc8f63f3f9f
Rendszerfejlesztes <Rendszerfejlesztes@lab000008>
2024-04-24 10:57:24 +0000

    Add project files.

---
+++ kliens_alkalmazas/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Hotcakes.Commerce;
+
+namespace kliens_alkalmazas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+        }
+            
+    }
+
+   
+}
