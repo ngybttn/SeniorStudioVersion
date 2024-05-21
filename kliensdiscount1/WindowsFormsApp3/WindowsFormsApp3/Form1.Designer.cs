f6db00afe0445096213e3fdbf769727cddda64a5
borbron <118373618+borbron@users.noreply.github.com>
2024-05-21 15:12:40 +0200

    Add files via upload

---
+++ kliensdiscount1/WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.buttonUndo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDiscount = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Bisque;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 163);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(831, 417);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.BackColor = System.Drawing.Color.Bisque;
+            this.buttonApply.Font = new System.Drawing.Font("Roboto", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonApply.Location = new System.Drawing.Point(726, 589);
+            this.buttonApply.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(150, 66);
+            this.buttonApply.TabIndex = 2;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = false;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // buttonUndo
+            // 
+            this.buttonUndo.BackColor = System.Drawing.Color.Bisque;
+            this.buttonUndo.Font = new System.Drawing.Font("Roboto", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUndo.Location = new System.Drawing.Point(45, 589);
+            this.buttonUndo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonUndo.Name = "buttonUndo";
+            this.buttonUndo.Size = new System.Drawing.Size(150, 66);
+            this.buttonUndo.TabIndex = 3;
+            this.buttonUndo.Text = "Undo";
+            this.buttonUndo.UseVisualStyleBackColor = false;
+            this.buttonUndo.Click += new System.EventHandler(this.buttonUndo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Bisque;
+            this.label1.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Discount products by selected %";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Bisque;
+            this.label2.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(582, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select products you want to discount and click the \'Apply\' button!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Bisque;
+            this.label3.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 104);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(487, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Click \'Undo\' to recover (some) of the discounted price.";
+            // 
+            // comboBoxDiscount
+            // 
+            this.comboBoxDiscount.BackColor = System.Drawing.Color.Bisque;
+            this.comboBoxDiscount.Font = new System.Drawing.Font("Roboto", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxDiscount.FormattingEnabled = true;
+            this.comboBoxDiscount.Items.AddRange(new object[] {
+            "10%",
+            "15%",
+            "20%",
+            "25%",
+            "50%"});
+            this.comboBoxDiscount.Location = new System.Drawing.Point(772, 29);
+            this.comboBoxDiscount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxDiscount.Name = "comboBoxDiscount";
+            this.comboBoxDiscount.Size = new System.Drawing.Size(102, 27);
+            this.comboBoxDiscount.TabIndex = 7;
+            this.comboBoxDiscount.Text = "10%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(912, 686);
+            this.Controls.Add(this.comboBoxDiscount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonUndo);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Product Discount - Greatest Games";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button buttonApply;
+		private System.Windows.Forms.Button buttonUndo;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBoxDiscount;
+	}
+}
+
---
+++ kliensdiscount1/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,151 @@
+﻿using Hotcakes.CommerceDTO.v1.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+	public partial class Form1 : Form
+	{
+		private readonly Hotcakes.CommerceDTO.v1.Client.Api _proxy;
+		private List<ProductDTO> _allProducts;
+		private Dictionary<ProductDTO, decimal> originalPrices = new Dictionary<ProductDTO, decimal>();
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			string url = "http://20.234.113.211:8103";
+			string key = "1-9777f99c-010f-461a-9d1c-c851faf6dcae";
+
+			_proxy = new Hotcakes.CommerceDTO.v1.Client.Api(url, key);
+
+			LoadProducts();
+			
+		}
+
+		private void LoadProducts()
+		{
+			Hotcakes.CommerceDTO.v1.ApiResponse<List<ProductDTO>> response = _proxy.ProductsFindAll();
+
+			if (response != null && response.Content != null)
+			{
+				_allProducts = response.Content;
+
+				
+				dataGridView1.DataSource = _allProducts.Select(p => new { p.ProductName, SitePrice = Math.Round(p.SitePrice, 2) }).ToList();
+
+				
+				dataGridView1.Columns[0].HeaderText = "Product Name";
+				dataGridView1.Columns[1].HeaderText = "Site Price";
+
+				
+				dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			}
+			else
+			{
+				MessageBox.Show("Failed to fetch products or the response is empty. Check your API configuration.");
+			}
+		}
+
+		private void buttonApply_Click(object sender, EventArgs e)
+		{
+			
+			DataGridViewSelectedRowCollection selectedRows = dataGridView1.SelectedRows;
+
+			if (selectedRows.Count == 0)
+			{
+				MessageBox.Show("Please select at least one product to apply the discount.");
+				return;
+			}
+
+			
+			int discountPercentage = int.Parse(comboBoxDiscount.SelectedItem.ToString().Replace("%", ""));
+
+			
+			foreach (DataGridViewRow row in selectedRows)
+			{
+				ProductDTO product = _allProducts[row.Index];
+
+				
+				if (!originalPrices.ContainsKey(product))
+				{
+					originalPrices.Add(product, product.SitePrice);
+				}
+
+				
+				decimal discountMultiplier = (100 - discountPercentage) / 100.0m;
+				product.SitePrice *= discountMultiplier;
+
+				
+				try
+				{
+					var response = _proxy.ProductsUpdate(product);
+
+					if (response != null)
+					{
+						MessageBox.Show("Discount applied successfully.");
+					}
+					else
+					{
+						MessageBox.Show("Failed to apply discount.");
+						
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"An error occurred: {ex.Message}");
+				}
+			}
+
+			// Refresh DataGridView to reflect changes
+			LoadProducts();
+		}
+
+
+		private void buttonUndo_Click(object sender, EventArgs e)
+		{
+			
+			string selectedDiscount = comboBoxDiscount.SelectedItem.ToString().Trim('%');
+			decimal discountPercentage = Convert.ToDecimal(selectedDiscount) / 100;
+
+			foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+			{
+				ProductDTO product = _allProducts[row.Index];
+
+				
+				product.SitePrice /= (1 - discountPercentage);
+
+				try
+				{
+					var response = _proxy.ProductsUpdate(product);
+
+					if (response != null)
+					{
+						MessageBox.Show("Discount reverted successfully.");
+					}
+					else
+					{
+						MessageBox.Show("Failed to revert discount.");
+						
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"An error occurred: {ex.Message}");
+				}
+			}
+
+			// Refresh DataGridView to reflect changes
+			LoadProducts();
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+	}
+}
